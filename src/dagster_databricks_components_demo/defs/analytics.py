75536286f39681927c2f699002d9fb9b9033b750
Colton Padden <colton@dagsterlabs.com>
2025-07-22 15:52:35 -0400

    wip

--- src/dagster_databricks_components_demo/defs/analytics.py
+++ src/dagster_databricks_components_demo/defs/analytics.py
@@ -3,6 +3,7 @@ import dagster as dg
 
 @dg.asset(
     kinds={"looker"},
+    owners=["chuck@acme.com"],
     deps=["account_performance", "customer_insights", "transactions"],
 )
 def analytics_dashboard(context: dg.AssetExecutionContext) -> dg.MaterializeResult: ...
--- src/dagster_databricks_components_demo/defs/assets.py
+++ src/dagster_databricks_components_demo/defs/assets.py
@@ -1,24 +1,26 @@
 import dagster as dg
 
+ASSET_OWNERS = ["bob@acme.com"]
 
-@dg.asset(kinds={"s3", "csv"})
+
+@dg.asset(kinds={"s3", "csv"}, owners=ASSET_OWNERS)
 def raw_transactions(context: dg.AssetExecutionContext) -> dg.MaterializeResult: ...
 
 
-@dg.asset(kinds={"s3", "csv"})
+@dg.asset(kinds={"s3", "csv"}, owners=ASSET_OWNERS)
 def raw_customers(context: dg.AssetExecutionContext) -> dg.MaterializeResult: ...
 
 
-@dg.asset(kinds={"s3", "csv"})
+@dg.asset(kinds={"s3", "csv"}, owners=ASSET_OWNERS)
 def raw_accounts(context: dg.AssetExecutionContext) -> dg.MaterializeResult: ...
 
-@dg.asset(kinds={"s3", "parquet"}, deps=[raw_transactions])
+@dg.asset(kinds={"s3", "parquet"}, deps=[raw_transactions], owners=ASSET_OWNERS)
 def prepared_transactions(context: dg.AssetExecutionContext) -> dg.MaterializeResult: ...
 
 
-@dg.asset(kinds={"s3", "parquet"}, deps=[raw_customers])
+@dg.asset(kinds={"s3", "parquet"}, deps=[raw_customers], owners=ASSET_OWNERS)
 def prepared_customers(context: dg.AssetExecutionContext) -> dg.MaterializeResult: ...
 
 
-@dg.asset(kinds={"s3", "parquet"}, deps=[raw_accounts])
+@dg.asset(kinds={"s3", "parquet"}, deps=[raw_accounts], owners=ASSET_OWNERS)
 def prepared_accounts(context: dg.AssetExecutionContext) -> dg.MaterializeResult: ...
